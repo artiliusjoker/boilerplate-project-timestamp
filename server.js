46ab604452cd88cc4dd0a7c5643ef7fdf3ddf84b
Minh Quan <48386342+artiliusjoker@users.noreply.github.com>
2021-12-06 11:06:02 +0700

    add timestamp

--- server.js
+++ server.js
@@ -2,31 +2,45 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
+// get timestamp
+app.get("/api/:inputDate", function (req, res) {
+  const input = /^[0-9]*$/.test(req.params.inputDate)
+    ? Number(req.params.inputDate)
+    : req.params.inputDate;
+  const inputDate = new Date(input);
 
+  if (isNaN(inputDate)) {
+    res.json({ error: "Invalid Date" });
+    return;
+  }
+
+  const unix = inputDate.getTime();
+  const utc = inputDate.toUTCString();
+  res.json({ unix: unix, utc: utc });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
